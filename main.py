a5e2fddf0eead25bf6aa459ad7d68c0ffda84bbf
zzffvcd <gleb2008.k@yandex.ru>
2023-11-24 12:52:36 +0300

    3-ay

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QPushButton
 from random import randint
 
 
-class Example(QMainWindow):
+class Design(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.resize(650, 420)
+        self.btn = QPushButton('Жмякать', self)
+        self.btn.move(50, 50)
+
+class Example(Design):
+    def __init__(self):
+        super().__init__()
         self.flag = False
-        self.pushButton.clicked.connect(self.go_paint)
+        self.btn.clicked.connect(self.go_paint)
 
     def go_paint(self):
         self.qp = QPainter()
@@ -24,8 +33,9 @@ class Example(QMainWindow):
             self.qp.end()
 
     def draw_flag(self, qp):
-        self.qp.setBrush(QColor(255, 255, 0))
-        x, y = randint(0, 549), randint(0, 549)
+        r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+        self.qp.setBrush(QColor(r, g, b))
+        x, y = randint(0, 650), randint(0, 420)
         d = randint(10, 30)
         self.qp.drawEllipse(x, y, d, d)
 
